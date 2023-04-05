25a88f9f01a5192dc1f92b9f17166978923f8e56
Abhishek More <abhishekmore021@gmail.com>
2023-04-05 09:55:11 +0530

    adding gitignore file

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3500;
 
-app.use("/", express.static(path.join(__dirname, "/public")));
-
+app.use(express.json())
+app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
 
 app.all("*", (req, res) => {
