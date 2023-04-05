d7da0d15dc137bde710f2f6c3a15e684bb29130f
Abhishek More <abhishekmore021@gmail.com>
2023-04-05 10:37:28 +0530

    setting up CORS middleware

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const { logger } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
+// const corsOption = require("./config/corsOption");
 
 const app = express();
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
 
